9aa49a92fae9b448ea204766b95db1f1f8d7aff3
Feng Kuok <fengkuok@gmail.com>
2013-01-19 13:37:11 +0800

    active record web监听方式启动

---
+++ modules/extension/src/main/java/org/makersoft/activerecord/bootstrap/ContextLoaderListener.java
@@ -0,0 +1,21 @@
+/*
+ * @(#)ActiveRecordContextLoaderListener.java 2013-1-19 下午1:14:22
+ *
+ * Copyright (c) 2011-2013 Makersoft.org all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ */
+package org.makersoft.activerecord.bootstrap;
+
+
+/**
+ * web application listener.
+ * 	 
+ * @version 2013-1-19 下午1:14:22
+ * @author Feng Kuok
+ * @see ServletContextListener
+ */
+public class ContextLoaderListener {
+
+}
