5ae4751c24befddfd9fa7a14d3f89a5489a33e6a
Feng Kuok <fengkuok@gmail.com>
2013-02-06 14:06:37 +0800

    test

---
+++ modules/extension/src/test/java/org/makersoft/test/unit/UnitTests.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)UnitTests.java 2013-1-25 下午3:11:15
+ *
+ * Copyright (c) 2011-2013 Makersoft.org all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ */
+package org.makersoft.test.unit;
+
+import org.junit.Test;
+import org.makersoft.test.UnitTest;
+
+/**
+ * test
+ */
+public class UnitTests extends UnitTest{
+
+	@Test(timeout = 1001)
+	public void testAssert() throws Exception{
+		super.pause(1000);
+	}
+}
