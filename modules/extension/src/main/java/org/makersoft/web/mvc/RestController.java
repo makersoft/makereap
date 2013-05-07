05cbd20934ced937dbf05215329a9be43d2c0e50
fengkuok <fengkuok@gmail.com>
2013-05-07 10:38:24 +0800

    Rest controller super class for future

---
+++ modules/extension/src/main/java/org/makersoft/web/mvc/RestController.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)RestController.java 2013-1-28 下午3:25:17
+ *
+ * Copyright (c) 2011-2013 Makersoft.org all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ */
+package org.makersoft.web.mvc;
+
+/**
+ * RestController.
+ */
+public abstract class RestController {
+	
+	public static final String index = "index";
+	public static final String edit = "edit";
+	public static final String _new = "_new";
+	
+//	public String get 
+	
+
+}
